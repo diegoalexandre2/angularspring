369d404029908fb7ba92da1af22ea7a619562a88
Alexandre  Andrade <79598607+diegoalexandre2@users.noreply.github.com>
2023-01-25 01:43:33 -0300

    testdtdabase

--- crud-spring/src/main/java/com/diego/crudspring/repository/CursoRepository.java
+++ crud-spring/src/main/java/com/diego/crudspring/repository/CursoRepository.java
@@ -1,11 +1,9 @@
 package com.diego.crudspring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.diego.crudspring.model.Cursos;
 
-@Repository
 public interface CursoRepository extends JpaRepository<Cursos, Long>{
 
 
